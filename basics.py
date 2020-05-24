c9c70ee1c5ffccfa130d7b4d803cea8e7a7d8d7a
moonquay <growyourownfood@gmail.com>
2020-05-24 12:43:38 -0600

    added explanatory strings to calculations output

--- basics.py
+++ basics.py
@@ -2,8 +2,10 @@ def mean(x):
     """Find the mean of an input of numbers"""
     x_mean = sum(x)/len(x)
     return x_mean
+    # print('The mean is: ' + str(x_mean))
 
 def error(list, y):
     """Subtracts an input value from a list of numbers"""
     y_errors = [z - y for z in list]
     return y_errors
+    # print('The error is: ' + str(y_errors))
--- calculations.py
+++ calculations.py
@@ -1,5 +1,7 @@
 from basics import mean, error
 
 my_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-print(mean(my_list))
-print(error(my_list, 4))
+print("Starting with this list of numbers: " + str(my_list))
+print("The mean is: " + str(mean(my_list)))
+print("The error is: " + str(error(my_list, mean(my_list))))
+
