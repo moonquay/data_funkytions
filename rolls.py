fdc196da243010b6cdb356edf5aa044467d6c670
moonquay <growyourownfood@gmail.com>
2020-05-26 21:39:24 -0600

    copied dice() into file

--- rolls.py
+++ rolls.py
@@ -1,8 +1,17 @@
 
-def roll_probability(sides=6, rolls=10, target=6, number_of_targets=3, simulations=1000):
-    """Determine the probability of rolling 'number_of_target' rolls of 'target' number
+def dice(sides=6, rolls=10):
+    """Rolling dice of x sides y times."""
+    import random
+    dice_sides = [x for x in range(1, sides + 1)]
+    dice_rolls = []
+    for y in range(rolls):
+        roll_output = random.choice(dice_sides)
+        dice_rolls.append(roll_output)
+    return dice_rolls
+
+def roll_probability(sides=6, rolls=10, target=6, number_of_targets=3, simulations=5000):
+    """Determine the probability of rolling 'number_of_targets' rolls of 'target' number
     in 'rolls' rolls using a 'sides'-sided dice, simulated 'simulations' times."""
-    from dice import dice
     from collections import defaultdict
     succesful_sim = 0
     i = 0
@@ -15,8 +24,10 @@ def roll_probability(sides=6, rolls=10, target=6, number_of_targets=3, simulatio
         if num_target_rolls >= number_of_targets:
             succesful_sim += 1
         i += 1
-    probability = succesful_sim / simulations
-    print("There's about a " + str(probability * 100) + '% chance of a ' + str(sides) + '-sided die rolling a total of ' + str(number_of_targets) + ' "' + str(target) + '"s over ' + str(rolls) + ' rolls.')
+    probability = (succesful_sim / simulations) * 100
+    print("There's about a " + str(probability) + '% chance of a ' + str(sides) + '-sided die rolling a total of ' + str(number_of_targets) + ' "' + str(target) + '"s over ' + str(rolls) + ' rolls.')
 
-roll_probability(sides=6, rolls=10, target=6, number_of_targets=7, simulations=5000)
-roll_probability(sides=250, rolls=198, target=15, number_of_targets=3, simulations=5000)
+# Run program over roll probabilities of interest
+roll_probability(sides=6, rolls=10, target=6, number_of_targets=7)
+roll_probability(sides=250, rolls=198, target=15, number_of_targets=3)
+
